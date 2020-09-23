8a3c99dbeff01037e01eac1ccd8aefb2487575ba
Yanling Wang <yuw17@psu.edu>
2020-09-23 15:27:11 -0400

    hw4.c adding comments

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -1,15 +1,25 @@
+// given a positive int n, return number of divisors for n between [1,n] inclusive 
+// You must use a while-loop for this function
 int num_of_divisors(int n) {
   return 0;
 }
 
+// given a positive int n, return number of primes that is <= n.
+// You must use num_of_divisors() function to help determine if a number is prime:
+// a number is a prime if its number of divisors is 2.
+// You must use a while-loop for this function
 int num_of_primes(int n) {
   return 0;
 }
 
+// given a non-negative int n, return the sum 0+1+2+...+n
+// You must use a while-loop for this function, no recursion.
 int sum_n(int n) {
   return 0;
 }
 
+// given a non-negative int n, print s n times
+// You must use a while-loop for this function, no recursion.
 void print_n(const char *s, int n) {
   return;
 }
