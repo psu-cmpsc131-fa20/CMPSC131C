4883cbf76597ca6618a118230a5b0b6b6da538ff
Yanling Wang <yuw17@psu.edu>
2020-09-23 15:22:36 -0400

    change setup.sh source to CMPSC131C

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-git remote add staff https://github.com/psu-cmpsc131-fa20/CMPSC131PYTHON.git
+git remote add staff https://github.com/psu-cmpsc131-fa20/CMPSC131C.git 
 git pull staff master --allow-unrelated-histories
