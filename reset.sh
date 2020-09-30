cdbe5bd1ebd8023ba506652cefd0235f56faaea5
Asmit De <asmit.de@outlook.com>
2020-09-30 00:29:22 -0400

    Backup old code, integrate setup and update.

--- reset.sh
+++ reset.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 echo "Initializing reset of cmpsc131c repository..."
 
-echo "Stashing all uncommitted changes..."
-git stash
+echo "Creating backup..."
+git add .
+git commit -m "All your old stuff"
+git checkout -b backup
+git push origin backup
 echo "...done"
 
 echo "Rolling back commits..."
 git checkout 75f59aa2f5ee3d251985a1763f780f33476ac9f5
 git branch -D master
-git branch master
-git checkout master
+git checkout -b master
+echo "...done"
+
+echo "Running setup..."
+git remote add staff https://github.com/psu-cmpsc131-fa20/CMPSC131C.git
+git pull staff master --allow-unrelated-histories
+echo "...done"
+
+echo "Running update..."
+git pull staff master --allow-unrelated-histories
+git push -u origin -f master
 echo "...done"
