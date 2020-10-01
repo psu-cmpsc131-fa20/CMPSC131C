e01e4b9a26b30804424d5495b49fe774f65d3ed1
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-09-30 21:53:23 -0400

    Update quiz1.c

--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -1,5 +1,6 @@
 // Author: 
 #include "quiz1.h"
 int digit_sum_base(int n, int base) {
+  prinf("How am I being quizzed on something we didn't learn in class? How am I supposed to write code for that? Thanks...")
   return 0;
 }
