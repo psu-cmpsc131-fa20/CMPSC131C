fd0de14f2c5434c9ea30817bbda989351e8777f5
Asmit De <asmit.de@outlook.com>
2020-10-02 00:01:45 -0400

    Added timestamp to backup branch. Supressed detached head message

--- reset.sh
+++ reset.sh
@@ -4,14 +4,14 @@ echo "Initializing reset of cmpsc131c repository..."
 echo "Creating backup..."
 git add .
 git commit -m "All your old stuff"
-git checkout -b backup
-git push origin backup
+branch=backup.$(date +"%s")
+git checkout -b $branch
+git push origin $branch
 echo "...done"
 
 echo "Rolling back commits..."
-git checkout 75f59aa2f5ee3d251985a1763f780f33476ac9f5
 git branch -D master
-git checkout -b master
+git checkout -b master 75f59aa2f5ee3d251985a1763f780f33476ac9f5
 echo "...done"
 
 echo "Running setup..."
