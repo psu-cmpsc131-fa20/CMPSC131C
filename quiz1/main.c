56c9173a770a4595112203eb5bba6c8d4a608e59
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-10-02 20:32:57 -0400

    Update main.c

--- quiz1/main.c
+++ quiz1/main.c
@@ -3,5 +3,8 @@
 #include "quiz1.h"
 
 int main(void) {
-  return 0;
+  z = readline("Enter an integer: ");
+  bas3 = readline("Enter a base: ");
+  f = quiz1(z, int bas3);
+  printf("answer is %d.",f);
 }
