7d4ed67a87bbe75b60cb5b2345b88eee7344b1ce
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-10-02 20:39:08 -0400

    Update quiz1.c

--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -1,14 +1,15 @@
 // Author: 
 #include "quiz1.h"
 #include <readline/readline.h>
+#include <stdlib.h>
+
 int digit_sum_base(int z, int bas3) {
   if (z > 0):
-    bas3 = readline("Enter a base: ");
     digit_sum_base(z/bas3);
     zf = n%bas3;
     printf("answer is %d.",zf);
     x = x+1;
     
     
-  return 0;
+  return zf;
 }
